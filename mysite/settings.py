77d51742bca7fd94016c4b7ef57e621b8dad49e7
aburender <aburender@aburender.com>
2023-01-08 23:17:25 -0400

    csrf allowed host add

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,6 +27,7 @@ SECRET_KEY = 'django-insecure-rc^*w^w&6g9_(uvx#6s*bnt!w)l0rdi%!l7mv#y%uc&x%wo5pk
 DEBUG = os.environ.get('modo', '') != 'produccion'
 
 ALLOWED_HOSTS = ["*"]
+CSRF_COOKIE_DOMAIN = ['paccarlos.up.railway.app']
 
 # Application definition
 
